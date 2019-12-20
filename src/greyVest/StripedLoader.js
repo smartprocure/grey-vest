dff7c2bfac7c31704ea7370bfc36367de188d959
Michelle Saad <git@michsa.me>
2019-12-19 23:35:32 -0500

    kill more things

--- src/greyVest/StripedLoader.js
+++ src/greyVest/StripedLoader.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import { loading as loadingStyle } from '../styles/generic'
+
+let loadingStyle = {
+  backgroundImage:
+    'linear-gradient(-45deg, rgba(200, 200, 200, 0.2) 25%, transparent 25%, transparent 50%, rgba(200, 200, 200, 0.2) 50%, rgba(200, 200, 200, 0.2) 75%, transparent 75%, transparent',
+  backgroundSize: '1rem 1rem',
+  opacity: 0.5,
+}
 
 let StripedLoader = ({ loading, style = {}, children }) => (
   <div style={{ ...style, ...(loading && loadingStyle) }}>{children}</div>
