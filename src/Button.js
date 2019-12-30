ff63130e51b1499d26ca69d53f19e97bc876d854
Michelle Saad <git@michsa.me>
2019-12-30 01:54:29 -0500

    do the buttons

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,87 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import styled from '@emotion/styled'
+import { Subtitle, Text } from './'
+import theme from './theme'
+import { defaultProps } from 'recompose'
 
-let Button = ({ isActive, primary, as: As = 'button', className, ...x }) => (
+let ButtonText = ({ compact = false, large = false, ...props }) =>
+  compact ? (
+    <Text extraSmall css={{ fontWeight: 'bold' }} {...props} />
+  ) : (
+    <Subtitle large={large} {...props} />
+  )
+
+let BaseButton = ({
+  as: As = 'button',
+  compact = false,
+  large = false,
+  children,
+  ...props
+}) => (
   <As
-    className={`gv-button ${isActive ? 'active' : ''} ${
-      primary ? 'primary' : ''
-    } ${className || ''}`}
-    {...x}
-  />
+    css={{
+      padding: large
+        ? theme.spaces[4]
+        : compact
+        ? `${theme.spaces[1]}px ${theme.spaces[2]}px`
+        : `${theme.spaces[2]}px ${theme.spaces[4]}px`,
+    }}
+    {...props}
+  >
+    <ButtonText {...{ compact, large }}>{children}</ButtonText>
+  </As>
 )
+
+let Button = styled(BaseButton)(
+  {
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+    borderRadius: 3,
+    transition: 'background-color .2s linear',
+    backgroundColor: theme.colors.neutrals[4],
+    ':hover': { backgroundColor: theme.colors.neutrals[5] },
+    ':active': {
+      backgroundColor: theme.colors.neutrals[6],
+      transition: 'none',
+    },
+    '& > *': { color: theme.colors.secondaries[1] },
+  },
+  ({ disabled }) =>
+    disabled && {
+      cursor: 'not-allowed',
+      opacity: 0.5,
+    }
+)
+Button.Secondary = Button
+
+Button.Primary = styled(Button)({
+  backgroundColor: theme.colors.primaries[0],
+  ':hover': { backgroundColor: theme.colors.primaries[1] },
+  ':active': { backgroundColor: theme.colors.primaries[2] },
+  '& > *': { color: theme.colors.neutrals[0] },
+})
+
+Button.Tertiary = styled(Button)({
+  backgroundColor: theme.colors.secondaries[1],
+  ':hover': { backgroundColor: theme.colors.secondaries[0] },
+  ':active': { backgroundColor: theme.colors.secondaries[2] },
+  '& > *': { color: theme.colors.neutrals[0] },
+})
+
+Button.Danger = styled(Button)({
+  backgroundColor: theme.colors.errors[1],
+  ':hover': { backgroundColor: theme.colors.errors[2] },
+  ':active': { backgroundColor: theme.colors.errors[0] },
+  '& > *': { color: theme.colors.neutrals[0] },
+})
+
+Button.Ghost = styled(Button)({
+  backgroundColor: 'transparent',
+  ':hover': { backgroundColor: theme.colors.neutrals[4] },
+  ':active': { backgroundColor: theme.colors.neutrals[5] },
+  '& > *': { color: theme.colors.primaries[0] },
+})
+
 export default Button
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,44 +1,85 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { Button } from '..'
+import { Button, Grid } from '..'
 import decorator from './decorator'
 
 export default {
-  title: 'Components|Button',
+  title: 'Button',
   component: Button,
   decorators: [decorator],
 }
 
-export let basicUsage = () => (
-  <Button variant="secondary" size="big" onClick={() => action('clicked')()}>Click</Button>
+let clickAction = () => action('clicked')()
+
+let Container = ({ cols = 3, ...props }) => (
+  <Grid
+    gap={2}
+    columns={`repeat(${cols}, auto)`}
+    placeItems="baseline"
+    {...props}
+  />
 )
 
-export let disabled = () => (
-  <Button disabled onClick={() => action('clicked')()}>
-    Don't click
-  </Button>
-)
-
-export let active = () => (
-  <Button isActive onClick={() => action('clicked')()}>
-    Click
-  </Button>
+export let regular = () => (
+  <Container>
+    <Button>Regular Button</Button>
+    <Button large>Large Button</Button>
+    <Button compact>Compact Button</Button>
+  </Container>
 )
 
 export let primary = () => (
-  <Button primary onClick={() => action('clicked')()}>
-    Click
-  </Button>
+  <Container>
+    <Button.Primary>Regular</Button.Primary>
+    <Button.Primary large>Large</Button.Primary>
+    <Button.Primary compact>Compact</Button.Primary>
+  </Container>
 )
 
-export let primaryDisabled = () => (
-  <Button primary disabled onClick={() => action('clicked')()}>
-    Can't touch this
-  </Button>
+export let secondary = () => (
+  <Container>
+    <Button.Secondary>Regular</Button.Secondary>
+    <Button.Secondary large>Large</Button.Secondary>
+    <Button.Secondary compact>Compact</Button.Secondary>
+  </Container>
+)
+secondary.story = { name: 'Secondary (default)' }
+
+export let tertiary = () => (
+  <Container>
+    <Button.Tertiary>Regular</Button.Tertiary>
+    <Button.Tertiary large>Large</Button.Tertiary>
+    <Button.Tertiary compact>Compact</Button.Tertiary>
+  </Container>
+)
+
+export let danger = () => (
+  <Container>
+    <Button.Danger>Regular</Button.Danger>
+    <Button.Danger large>Large</Button.Danger>
+    <Button.Danger compact>Compact</Button.Danger>
+  </Container>
+)
+
+export let ghost = () => (
+  <Container>
+    <Button.Ghost>Regular</Button.Ghost>
+    <Button.Ghost large>Large</Button.Ghost>
+    <Button.Ghost compact>Compact</Button.Ghost>
+  </Container>
+)
+
+export let disabled = () => (
+  <Container cols={4}>
+    <Button.Primary disabled>Primary</Button.Primary>
+    <Button.Secondary disabled>Secondary</Button.Secondary>
+    <Button.Tertiary disabled>Tertiary</Button.Tertiary>
+    <Button.Danger disabled>Danger</Button.Danger>
+  </Container>
 )
 
 export let asDiv = () => (
-  <Button as="div" onClick={() => action('clicked')()}>
+  <Button as="div" onClick={clickAction}>
     Click
   </Button>
 )
