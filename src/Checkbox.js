df0a9c53aa4099e108fe73621f507bf15d511d79
Michelle Saad <git@michsa.me>
2019-12-30 23:55:54 -0500

    CheckboxList & stories

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -5,51 +5,59 @@ import Flex from './Flex'
 import theme from './theme'
 
 // Low effort custom checkbox
-let Checkbox = ({ checked = false, disabled, onChange = () => {} }) => (
-    <Flex
+let Checkbox = ({
+  checked = false,
+  disabled,
+  onChange = () => {},
+  ...props
+}) => (
+  <Flex
     as="label"
-      css={[
-        {
-          boxSizing: 'border-box',
-          cursor: 'pointer',
-          height: theme.spaces[4],
-          width: theme.spaces[4],
-          borderRadius: 3,
-          backgroundColor: theme.colors.neutrals[0],
-          border: `2px solid ${theme.colors.neutrals[6]}`,
-          '& *': { color: theme.colors.primaries[0] },
-        },
-        disabled && {
-          borderColor: theme.colors.neutrals[5],
-          backgroundColor: theme.colors.neutrals[5],
-          opacity: 0.5,
-          cursor: 'not-allowed',
-        },
-      ]}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <input
-        type="checkbox"
-        style={{
-          border: 0,
-          clip: 'rect(0 0 0 0)',
-          clippath: 'inset(50%)',
-          height: 1,
-          margin: '-1px',
-          overflow: 'hidden',
-          padding: 0,
-          position: 'absolute',
-          whiteSpace: 'nowrap',
-          width: 1,
-        }}
-        {...{ checked, onChange }}
-      />
-      {checked ? (
-        <Icon icon="check" size={0} style={{ fontWeight: 900 }} />
-      ) : (
-        String.fromCharCode(160) // non-breaking space
-      )}
-    </Flex>
+    css={[
+      {
+        boxSizing: 'border-box',
+        cursor: 'pointer',
+        height: theme.spaces[4],
+        width: theme.spaces[4],
+        minWidth: theme.spaces[4],
+        minHeight: theme.spaces[4],
+        borderRadius: 3,
+        backgroundColor: theme.colors.neutrals[0],
+        border: `2px solid ${theme.colors.neutrals[6]}`,
+        '& *': { color: theme.colors.primaries[0] },
+      },
+      disabled && {
+        borderColor: theme.colors.neutrals[5],
+        backgroundColor: theme.colors.neutrals[5],
+        opacity: 0.5,
+        cursor: 'not-allowed',
+      },
+    ]}
+    alignItems="center"
+    justifyContent="center"
+    {...props}
+  >
+    <input
+      type="checkbox"
+      style={{
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        clippath: 'inset(50%)',
+        height: 1,
+        margin: '-1px',
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        whiteSpace: 'nowrap',
+        width: 1,
+      }}
+      {...{ checked, onChange }}
+    />
+    {checked ? (
+      <Icon icon="check" size={0} style={{ fontWeight: 900 }} />
+    ) : (
+      String.fromCharCode(160) // non-breaking space
+    )}
+  </Flex>
 )
 export default Checkbox
---
+++ src/stories/CheckboxList.stories.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { CheckboxList } from '..'
+import F from 'futil'
+import decorator from './decorator'
+
+export default {
+  title: 'CheckboxList',
+  decorators: [decorator],
+  component: CheckboxList,
+}
+
+export let baseStory = () => {
+  let values = React.useState([])
+  return (
+    <CheckboxList
+      {...F.domLens.targetBinding('value')(values)}
+      options={[
+        { label: 'Nulla et fringilla nisl', value: 1 },
+        { label: 'Aliquam eu bibendum eros', value: 2 },
+        {
+          label:
+            'Mauris auctor scelerisque erat quis imperdiet. Nulla consequat, velit tincidunt accumsan laoreet, metus est euismod libero, eget euismod nisl eros eget metus.',
+          value: 3,
+        },
+        { label: 'Phasellus at efficitur quam', value: 4 }
+      ]}
+    />
+  )
+}
