6e2b1ee3bda75d29e0f3bb62d5fc1288759fa77a
Michelle Saad <git@michsa.me>
2020-01-08 23:36:41 -0500

    fix Awaiter when error is a { message, code } object

--- src/Awaiter.js
+++ src/Awaiter.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import F from 'futil'
 
 let Awaiter = ({ promise, children }) =>
   promise.case({
     pending: () => <div>Loading...</div>,
-    rejected: error => <div>Ooops.. {error}</div>,
+    rejected: error => <div>Ooops.. {F.getOrReturn('message', error)}</div>,
     fulfilled: value => <div>{children(value)}</div>,
   })
 
