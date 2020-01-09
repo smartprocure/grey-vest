75d2fa4111bc111c23472be0f58cfa7eba3ea375
Michelle Saad <git@michsa.me>
2020-01-08 23:37:25 -0500

    add stories for Divider

---
+++ src/stories/Divider.stories.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Divider, Text, Flex, Grid } from '..'
+import _ from 'lodash/fp'
+import decorator from './decorator'
+import theme from '../theme'
+
+export default {
+  title: 'Divider',
+  decorators: [decorator],
+  component: Divider,
+}
+
+export let divider = () => (
+  <>
+    <Text>as above</Text>
+    <Divider />
+    <Text>so below</Text>
+  </>
+)
+
+export let vertical = () => (
+  <Flex>
+    <Text>two paths diverged</Text>
+    <Divider vertical />
+    <Text>in a lonely wood</Text>
+  </Flex>
+)
+
+export let withGrid = () => (
+  <Grid columns={3} style={{ textAlign: 'center' }}>
+    <Text as="span">head</Text>
+    <Divider vertical />
+    <Text as="span">shoulders</Text>
+    <Divider width={3} style={{ display: 'inline', textAlign: 'center' }} />
+    <Text as="span">knees</Text>
+    <Divider vertical />
+    <Text as="span">toes</Text>
+  </Grid>
+)
