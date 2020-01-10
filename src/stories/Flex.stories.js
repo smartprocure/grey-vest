f297037be324c57ef0db29dca34dcae31af49774
Michelle Saad <git@michsa.me>
2020-01-10 15:41:43 -0500

    convert Flex stories to csf

--- src/stories/Flex.stories.js
+++ src/stories/Flex.stories.js
@@ -1,36 +1,37 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { Flex } from '..'
+import decorator from './decorator'
 
-let FlexDemo = ({ style, ...props }) => (
-  <Flex
-    wrap
-    justifyContent="center"
-    style={{
-      backgroundColor: 'lightblue',
-      fontSize: '2em',
-      maxWidth: 300,
-      ...style,
-    }}
-    {...props}
-  >
-    <div>Item1</div>
-    <div>Item2</div>
-    <div>Item2</div>
-    <div>Item4</div>
-    <div>Item5</div>
-    <div>Item6</div>
+export default {
+  title: 'Flex',
+  component: Flex,
+  decorators: [decorator],
+}
+
+export let asButton = () => (
+  <Flex column alignItems="center">
+    <Flex
+      as="button"
+      wrap
+      justifyContent="center"
+      style={{
+        backgroundColor: 'lightblue',
+        fontSize: '2em',
+        maxWidth: 300,
+      }}
+    >
+      <div>Item1</div>
+      <div>Item2</div>
+      <div>Item2</div>
+      <div>Item4</div>
+      <div>Item5</div>
+      <div>Item6</div>
+    </Flex>
   </Flex>
 )
 
-storiesOf('Components|Flex', module)
-  .add('As button', () => (
-    <Flex column alignItems="center">
-      <FlexDemo as="button" />
-    </Flex>
-  ))
-  .add('No children', () => (
-    <Flex column alignItems="center">
-      <Flex />
-    </Flex>
-  ))
+export let noChildren = () => (
+  <Flex column alignItems="center">
+    <Flex />
+  </Flex>
+)
