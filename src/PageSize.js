0c07e1cbdcaf1a9e074e7c8514c05e681f65582c
Michelle Saad <git@michsa.me>
2020-01-24 12:30:44 -0500

    PageSize styling

--- src/PageSize.js
+++ src/PageSize.js
@@ -2,32 +2,35 @@
 import { jsx } from '@emotion/core'
 import _ from 'lodash/fp'
 import GVPagerItem from './PagerItem'
+import { Text } from './Typography'
 import Flex from './Flex'
+import theme from './theme'
 
 let PageSize = ({
   value,
   onChange = () => {},
-  sizeOptions = [20, 50, 100],
+  sizeOptions = [20, 50, 100, 250],
   PagerItem = GVPagerItem
-}) => {
-  let allSizeOptions = _.union([value], sizeOptions)
-  return (
-    <div>
-      View
-      {_.map(
-        size => (
-          <PagerItem
-            key={size}
-            active={size === value}
-            onClick={() => onChange(size)}
-          >
-            {value}
-          </PagerItem>
-        ),
-        allSizeOptions
-      )}
-    </div>
-  )
-}
+}) => (
+  <Flex alignItems="baseline">
+    <Text small css={{ marginRight: theme.spaces.xs }}>View</Text>
+    {_.map(
+      size => (
+        <PagerItem
+          key={size}
+          active={size === value}
+          onClick={() => onChange(size)}
+        >
+          {size}
+        </PagerItem>
+      ),
+      _.flow(
+        _.concat(value),
+        _.sortBy(_.identity),
+        _.sortedUniq
+      )(sizeOptions)
+    )}
+  </Flex>
+)
 
 export default PageSize
