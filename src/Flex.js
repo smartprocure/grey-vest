04da8262f0f574b532601cc956bf1c72115e8038
Michelle Saad <git@michsa.me>
2020-01-24 16:18:14 -0500

    use emotion styled component API for Flex

--- src/Flex.js
+++ src/Flex.js
@@ -1,28 +1,23 @@
-import React from 'react'
+import styled from '@emotion/styled'
 
-let Flex = ({
-  as: Component = 'div',
-  style,
-  alignItems,
-  alignContent,
-  justifyContent,
-  wrap = false,
-  column = false,
-  inline = false,
-  ...props
-}) => (
-  <Component
-    style={{
-      display: `${inline ? 'inline-' : ''}flex`,
-      flexWrap: wrap && 'wrap',
-      flexDirection: column && 'column',
-      alignItems,
-      justifyContent,
-      alignContent,
-      ...style,
-    }}
-    {...props}
-  />
+let Flex = styled.div(
+  ({
+    style,
+    alignItems,
+    alignContent,
+    justifyContent,
+    wrap = false,
+    column = false,
+    inline = false,
+  }) => ({
+    display: `${inline ? 'inline-' : ''}flex`,
+    flexWrap: wrap && 'wrap',
+    flexDirection: column && 'column',
+    alignItems,
+    justifyContent,
+    alignContent,
+    ...style,
+  })
 )
 
 export default Flex
