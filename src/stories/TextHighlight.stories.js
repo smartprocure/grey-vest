4be45c4363a183c4cd16b3cdc3aca38f346857c3
Michelle Saad <git@michsa.me>
2020-01-29 14:10:58 -0500

    add lipsum to TextHighlight story

--- src/stories/TextHighlight.stories.js
+++ src/stories/TextHighlight.stories.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { loremIpsum } from 'lorem-ipsum'
 import F from 'futil'
+import _ from 'lodash/fp'
 import { TextHighlight, TextInput, Text, Grid } from '..'
 
 export default { title: 'TextHighlight', component: TextHighlight }
 
-let lipsum =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+let text = _.times(() => loremIpsum({ units: 'paragraph' }), 3)
 
 export let usage = () => {
   let filter = React.useState('')
@@ -15,9 +16,16 @@ export let usage = () => {
         placeholder="Enter text to highlight..."
         {...F.domLens.value(filter)}
       />
-      <Text>
-        <TextHighlight text={lipsum} pattern={F.view(filter)} />
-      </Text>
+      <div>
+        {F.mapIndexed(
+          (t, i) => (
+            <Text as="p" key={i}>
+              <TextHighlight text={t} pattern={F.view(filter)} />
+            </Text>
+          ),
+          text
+        )}
+      </div>
     </Grid>
   )
 }
