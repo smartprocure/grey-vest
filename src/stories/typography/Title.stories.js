1919b9229f667123574624aa2dc6ee5ea08ad5c7
Michelle Saad <git@michsa.me>
2020-01-29 14:10:34 -0500

    add lipsum to typography stories

--- src/stories/typography/Title.stories.js
+++ src/stories/typography/Title.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { loremIpsum } from 'lorem-ipsum'
 import { Flex, Title } from '../..'
 import theme from '../../theme'
 import SizeInfo from './SizeInfo'
@@ -9,22 +10,19 @@ export default {
   component: Title,
 }
 
+let lipsum = count => loremIpsum({ count })
+
 export let regular = () => (
   <Flex alignItems="center">
     <SizeInfo config={fonts.Title} />
-    <Title>
-      Regular title. Used for page headers. Morbi sodales ipsum sapien, eget
-      condimentum lorem pharetra non.
-    </Title>
+    <Title>Regular title. Used for page headers. {lipsum()}</Title>
   </Flex>
 )
 
 export let largeVariant = () => (
   <Flex alignItems="center">
     <SizeInfo config={fonts.Title.variants.large} />
-    <Title large>
-      Large title. Used for extra-large callouts. Integer et vestibulum mi.
-    </Title>
+    <Title large>Large title. Used for extra-large callouts. {lipsum()}</Title>
   </Flex>
 )
 
@@ -32,9 +30,7 @@ export let smallVariant = () => (
   <Flex alignItems="center">
     <SizeInfo config={fonts.Title.variants.small} />
     <Title small>
-      Small title. Used for modal headers and tab titles. Sed ornare bibendum
-      nisl, sit amet finibus orci blandit eget. Sed rhoncus, tellus in maximus
-      blandit, sapien enim suscipit nisl, sed eleifend sapien velit id nulla.
+      Small title. Used for modal headers and tab titles. {lipsum(2)}
     </Title>
   </Flex>
 )
