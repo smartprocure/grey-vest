452b5af64fd5954c5af98df2397a51d9eda5cc86
Michelle Saad <git@michsa.me>
2020-02-03 11:22:32 -0500

    lipsum the Tooltip story

--- src/stories/Tooltip.stories.js
+++ src/stories/Tooltip.stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Tooltip, DropdownItem, Button, Grid } from '..'
+import { loremIpsum } from 'lorem-ipsum'
 
 export default { title: 'Tooltip', component: Tooltip }
 
@@ -9,9 +10,7 @@ export let basicUsage = () => (
     <Button data-tip="it is a button">is it a button?</Button>
     <DropdownItem
       icon="help_outline"
-      data-tip={`Quisque ex tellus, egestas id massa sit amet, vestibulum iaculis libero.
-      Quisque tempor ligula ac tincidunt mollis. Quisque consectetur viverra dictum. 
-      In hac habitasse platea dictumst.`}
+      data-tip={loremIpsum({ count: 2 })}
     >
       This dropdown item has a very long tooltip
     </DropdownItem>
