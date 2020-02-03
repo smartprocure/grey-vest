3712386daf4fffde47f1ac7ddd97b1388a9d8024
Michelle Saad <git@michsa.me>
2020-02-03 12:28:51 -0500

    add spacing demo

---
+++ src/stories/SpacingDemo.stories.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import F from 'futil'
+import _ from 'lodash/fp'
+import { loremIpsum } from 'lorem-ipsum'
+import { Grid, Divider, Flex, SpacedList, Select, Dynamic } from '..'
+
+export default { title: 'Demos | Spacing' }
+
+let inlineContent = F.mapIndexed(
+  (x, i) => (
+    <span key={i} style={{ backgroundColor: 'cyan' }}>
+      {x}
+    </span>
+  ),
+  _.times(loremIpsum, 8)
+)
+
+export let withInlineContent = () => {
+  let components = { Flex, Grid, SpacedList }
+  let component = React.useState('Flex')
+  return (
+    <>
+      <Select
+        options={F.autoLabelOptions(_.keys(components))}
+        {...F.domLens.value(component)}
+      />
+      <Divider m={2} />
+      <Dynamic component={components[F.view(component)]} gap={1}>
+        {inlineContent}
+      </Dynamic>
+    </>
+  )
+}
+
+let columnContent = F.mapIndexed(
+  (height, i) => (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      key={i}
+      style={{ border: '2px solid cyan', background: 'yellow', fontSize: 20, height }}
+    >
+      {i}
+    </Flex>
+  ),
+  _.times(() => _.random(1, 10) * 20, 7)
+)
+
+export let columns = () => {
+  let components = { Grid, SpacedList }
+  let component = React.useState('Grid')
+  return (
+    <>
+      <Select
+        options={F.autoLabelOptions(_.keys(components))}
+        {...F.domLens.value(component)}
+      />
+      <Divider m={2} />
+      <Dynamic
+        component={components[F.view(component)]}
+        // both Grid and SpacedList
+        gap={1}
+        // Grid property
+        columns={2}
+        // SpacedList properties
+        columnCount={2}
+        columnGap={2}
+      >
+        {columnContent}
+      </Dynamic>
+    </>
+  )
+}
+columns.story = {
+  parameters: {
+    docs: {
+      storyDescription: 
+`
+This story demonstrates the differences in the way the \`Grid\` and \`SpacedList\` components handle columns.
+
+
+`
+    },
+  },
+}
