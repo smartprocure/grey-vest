79b922777c4ca5c43252d6377820d65c5c84066f
Michelle Saad <git@michsa.me>
2020-02-04 12:36:02 -0500

    improve PageHeader story

--- src/stories/templates/PageHeader.stories.js
+++ src/stories/templates/PageHeader.stories.js
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 import React from 'react'
 import F from 'futil'
 import { Flex, Button, Subtitle, Dropdown, DropdownItem } from '../..'
@@ -6,32 +8,67 @@ import theme from '../../theme'
 export default { title: 'Demos | Page Header' }
 
 export let story = () => {
-  let open = React.useState(false)
-  let SubSection = ({ active, ...props }) => (
-    <Button.Ghost
-      compact
-      style={{ color: !active && theme.colors.secondaries[1] }}
+  let SubSection = ({ active, title, onClick, children, ...props }) => {
+    let open = React.useState(false)
+    return (
+      <div>
+        <Button.Ghost
+          compact
+          style={{ color: !active && theme.colors.secondaries[1] }}
+          icon={children && 'arrow_drop_down'}
+          onClick={() => {
+            F.flip(open)()
+            onClick()
+          }}
+          {...props}
+        >
+          {title}
+        </Button.Ghost>
+        {children && <Dropdown open={open}>{children}</Dropdown>}
+      </div>
+    )
+  }
+  let InfoButton = props => (
+    <Button
+      css={{
+        color: theme.colors.neutrals[0],
+        backgroundColor: theme.colors.infos[1],
+        paddingTop: 1,
+        paddingBottom: 1,
+      }}
       {...props}
     />
   )
+
   return (
     <Flex
-      inline
-      gap={1}
       alignItems="center"
-      style={{ backgroundColor: theme.colors.neutrals[1] }}
+      justifyContent="space-between"
+      style={{
+        backgroundColor: theme.colors.neutrals[1],
+        padding: `${theme.space(1)}px ${theme.space(2)}px`,
+        borderBottom: `1px solid ${theme.colors.neutrals[3]}`,
+      }}
     >
-      <Subtitle large>Section</Subtitle>
-      <SubSection active>Sub-section</SubSection>
-      <SubSection>Sub-section</SubSection>
-      <Dropdown open={open}>
-        <DropdownItem>Subsection item 1</DropdownItem>
-        <DropdownItem>Subsection item 2</DropdownItem>
-        <DropdownItem>Subsection item 3</DropdownItem>
-      </Dropdown>
-      <SubSection onClick={F.flip(open)} icon="keyboard_arrow_down">
-        Sub-section
-      </SubSection>
+      <Flex gap={1} alignItems="center">
+        <Subtitle large>Section</Subtitle>
+        <SubSection active title="Sub-section 1" />
+        <SubSection title="Sub-section 2" />
+        <SubSection title="Sub-section 3">
+          <DropdownItem>Subsection item 1</DropdownItem>
+          <DropdownItem>Subsection item 2</DropdownItem>
+          <DropdownItem>Subsection item 3</DropdownItem>
+        </SubSection>
+      </Flex>
+
+      <Flex gap={1}>
+        <InfoButton compact icon="map">
+          Product Tour
+        </InfoButton>
+        <InfoButton compact icon="chat">
+          Help Chat
+        </InfoButton>
+      </Flex>
     </Flex>
   )
 }
