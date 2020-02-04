7fb01fd7d4bfb72b0e8401a2f7f3ca81b4142f22
Michelle Saad <git@michsa.me>
2020-02-03 19:35:43 -0500

    re-add Form component

---
+++ src/Form.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { Grid, Button, Flex, Title, Text } from '.'
+import theme from './theme'
+
+let Form = props => (
+  <Flex css={{ width: theme.widths.lg }} column gap={2} {...props} />
+)
+
+Form.Header = ({ title, description, ...props }) => (
+  <div {...props}>
+    {title && (
+      <Title small as="div">
+        {title}
+      </Title>
+    )}
+    {description && <Text>{description}</Text>}
+  </div>
+)
+
+Form.Content = props => <Grid rowGap={2} columnGap={3} {...props} />
+
+Form.Footer = props => (
+  <Flex gap={1} css={{ paddingTop: theme.space(1) }} {...props}>
+    <Button.Primary>Submit</Button.Primary>
+    <Button.Secondary>Cancel</Button.Secondary>
+  </Flex>
+)
+
+export default Form
