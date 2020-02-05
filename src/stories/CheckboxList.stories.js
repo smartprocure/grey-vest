2b09437bf47397d5d3e803dbb80ef7bd99d48116
Michelle Saad <git@michsa.me>
2020-02-05 17:12:11 -0500

    add CheckboxList/RadioList stories for disabled options

--- src/stories/CheckboxList.stories.js
+++ src/stories/CheckboxList.stories.js
@@ -10,19 +10,25 @@ export default { title: 'CheckboxList', component: CheckboxList }
 let options = optionsFromArray(_.times(loremIpsum, 5))
 
 export let story = () => {
-  let values = React.useState([])
+  let values = React.useState([0, 1])
   return (
     <Grid gap={1}>
       <div>
         Selected: <b>[{_.join(', ', F.view(values))}]</b>
       </div>
-      <CheckboxList {...F.domLens.value(values)} options={options} />
+      <CheckboxList
+        {...F.domLens.value(values)}
+        options={[
+          ..._.initial(options),
+          { ..._.last(options), disabled: true },
+        ]}
+      />
     </Grid>
   )
 }
 
 export let columns = () => {
-  let values = React.useState([])
+  let values = React.useState()
   return (
     <Grid gap={1}>
       <div>
@@ -31,7 +37,31 @@ export let columns = () => {
       <CheckboxList
         {...F.domLens.value(values)}
         options={options}
-        columnCount={3}
+        columnCount={2}
+      />
+    </Grid>
+  )
+}
+columns.story = {
+  parameters: {
+    docs: {
+      storyDescription:
+        "CheckboxList uses SpacedList internally, so it also accepts all of that component's props (`columnCount`, `columnWidth`, `gap`, and `columnGap`).",
+    },
+  },
+}
+
+export let disabled = () => {
+  let values = React.useState([0, 1])
+  return (
+    <Grid gap={1}>
+      <div>
+        Selected: <b>[{_.join(', ', F.view(values))}]</b>
+      </div>
+      <CheckboxList
+        native
+        {...F.domLens.value(values)}
+        options={_.map(_.set('disabled', true), options)}
       />
     </Grid>
   )
