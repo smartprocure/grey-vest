ca43e32c2bdccbaeb5ac8fa720268429c9a1d985
Michelle Saad <git@michsa.me>
2020-02-10 04:05:34 -0500

    add react-async awaiter

--- src/Awaiter.js
+++ src/Awaiter.js
@@ -1,12 +1,74 @@
 import React from 'react'
-import { observer } from 'mobx-react'
 import F from 'futil'
+import _ from 'lodash/fp'
+import { rgba } from 'polished'
+import Async from 'react-async'
+import { Modal, ModalHeader, ModalContent, ModalFooter } from './Modal'
+import Button from './Button'
+import LoadingSpinner from './StripedLoader'
+import CommandButton, { Command } from './CommandButton'
+import theme from './theme'
 
-let Awaiter = ({ promise, children }) =>
-  promise.case({
-    pending: () => <div>Loading...</div>,
-    rejected: error => <div>Ooops.. {F.getOrReturn('message', error)}</div>,
-    fulfilled: value => <div>{children(value)}</div>,
-  })
+let Error = ({ error: { message: errors = [] } }) => {
+  let toggle = React.useState(false)
+  return (
+    <>
+      <Button danger onClick={F.on(toggle)}>
+        Error!
+      </Button>
+      <Modal open={toggle}>
+        <ModalHeader>An error has occurred!</ModalHeader>
+        <ModalContent>
+          {F.mapIndexed(
+            (error, index) => (
+              <pre
+                key={index}
+                style={{
+                  backgroundColor: rgba(theme.colors.error, 0.1),
+                  overflowX: 'auto',
+                  color: theme.colors.error,
+                  margin: `${theme.spaces.xs}px 0`,
+                  padding: `0 ${theme.spaces.sm}px`,
+                }}
+              >
+                {error}
+              </pre>
+            ),
+            _.castArray(errors)
+          )}
+        </ModalContent>
+        <ModalFooter>
+          <Button onClick={F.off(toggle)}>Close</Button>
+          <CommandButton
+            primary
+            command={Command(() =>
+              navigator.clipboard.writeText(_.join('\n', errors))
+            )}
+          >
+            Copy Text
+          </CommandButton>
+        </ModalFooter>
+      </Modal>
+    </>
+  )
+}
 
-export default observer(Awaiter)
+let AsyncDefaults = ({
+  promiseFn,
+  LoadingComponent = LoadingSpinner,
+  children,
+  ErrorComponent = Error,
+  ...awaiterProps
+}) => (
+  <Async promiseFn={promiseFn} {...awaiterProps}>
+    {({ data, error, isLoading, ...props }) => {
+      if (isLoading) return <LoadingComponent />
+      if (error) {
+        console.error('AsyncDefaults error', error)
+        return <ErrorComponent error={error} />
+      }
+      return children(data, props)
+    }}
+  </Async>
+)
+export default AsyncDefaults
