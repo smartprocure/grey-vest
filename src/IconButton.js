32c449c298f5995d766fe08b1231fff65c86f56c
Michelle Saad <git@michsa.me>
2020-02-10 11:32:19 -0500

    fix IconButton & story

--- src/IconButton.js
+++ src/IconButton.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import TextButton from './TextButton'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 import Icon from './Icon'
+import theme from './theme'
 
 let IconButton = ({ icon, ...props }) => (
-  <TextButton {...props}>
+  <div
+    css={{
+      width: theme.spaces.lg,
+      height: theme.spaces.lg,
+      borderRadius: 100,
+      cursor: 'pointer',
+      display: 'inline-flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      transition: 'all .1s linear',
+      color: theme.colors.secondary,
+      backgroundColor: 'transparent',
+      opacity: 0.5,
+      ':hover': {
+        backgroundColor: theme.colors.neutrals[0],
+        opacity: 1,
+      },
+      '&:active, &.primary': {
+        backgroundColor: theme.colors.neutrals[1],
+      },
+    }}
+    {...props}
+  >
     <Icon icon={icon} />
-  </TextButton>
+  </div>
 )
 
 export default IconButton
--- src/stories/IconButton.stories.js
+++ src/stories/IconButton.stories.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { Icon, TextButton, IconButton, Flex } from '..'
+import { Icon, IconButton, Flex } from '..'
 
 export default { title: 'IconButton', component: IconButton }
 
 export let story = () => (
   <Flex justifyContent="space-around">
-    <Icon icon="TableColumnMenu" />
     <Icon icon="more_vert" />
-    <TextButton>
-      <Icon icon="more_vert" />
-    </TextButton>
     <IconButton icon="more_vert" />
-    <IconButton icon="TableColumnMenu" />
+    <IconButton icon="more_vert" />
+    <IconButton icon={() => <div>💃</div>} />
   </Flex>
 )
