af8659e0cfbd20bf5b56c577569640a7f57b615e
Michelle Saad <git@michsa.me>
2020-02-10 17:15:43 -0500

    add react-popper popup

--- src/Popover.js
+++ src/Popover.js
@@ -1,36 +1,51 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import _ from 'lodash/fp'
-import OutsideClickHandler from 'react-outside-click-handler'
-import { observer } from 'mobx-react'
+import { useState } from 'react'
+import F from 'futil'
 import Box from './Box'
-import theme from './theme'
-import { openBinding, expandProp } from './utils'
 
-let makePopover = padding =>
-  _.flow(
-    expandProp('open', openBinding),
-    observer
-  )(
-    ({ isOpen, onClose = _.noop, ...props }) =>
-      isOpen && (
-        <OutsideClickHandler onOutsideClick={_.debounce(0, onClose)}>
-          <Box popup
-            {...padding}
-            css={{
-              position: 'absolute',
-              maxWidth: theme.widths.popup.max,
-              minWidth: theme.widths.popup.min,
-            }}
-            {...props}
-          />
-        </OutsideClickHandler>
-      )
+import { Manager, Reference, Popper } from 'react-popper'
+import Button from './Button'
+import IconButton from './IconButton'
+
+export let Popover = ({
+  label = 'dropdown',
+  side = 'left',
+  trigger = 'button',
+  triggerProps,
+  children,
+}) => {
+  let open = useState(false)
+  let allTriggerProps = {
+    onClick: F.flip(open),
+    icon:
+      trigger === 'button'
+        ? `arrow_drop_${F.view(open) ? 'up' : 'down'}`
+        : 'more_vert',
+    ...F.callOrReturn(triggerProps, F.view(open)),
+  }
+  let Trigger = trigger === 'button' ? Button : IconButton
+  return (
+    <Manager>
+      <Reference>
+        {({ ref }) => (
+          <Trigger {...{ ref, ...allTriggerProps }}>{label}</Trigger>
+        )}
+      </Reference>
+      <Popper placement={`bottom-${side === 'right' ? 'end' : 'start'}`}>
+        {({ ref, style }) =>
+          F.view(open) && (
+            <Box
+              popup
+              padding={0}
+              css={{ marginTop: 4 }}
+              {...{ ref, style, children }}
+            />
+          )
+        }
+      </Popper>
+    </Manager>
   )
+}
 
-let Popover = makePopover({ px: 2, py: 1 })
-Popover.displayName = 'Popover'
-export default Popover
-
-export let Dropdown = makePopover({ p: 0 })
-Dropdown.displayName = 'Dropdown'
+export let Dropdown = Popover
--- src/stories/Dropdown.stories.js
+++ src/stories/Dropdown.stories.js
@@ -1,59 +1,39 @@
 import React from 'react'
-import F from 'futil'
-import { observable } from 'mobx'
-import { Button, Dropdown, DropdownItem, Divider } from '..'
+import { Dropdown, DropdownItem, Divider } from '..'
 
 export default { title: 'Dropdown', component: Dropdown }
 
-let ButtonDropdown = ({ children }) => {
-  let open = observable.box(false)
-  return (
-    <div style={{ height: 240 }}>
-      <Button icon="keyboard_arrow_down" onClick={F.on(open)}>
-        dropdown
-      </Button>
-      <Dropdown open={open}>{children}</Dropdown>
-    </div>
-  )
-}
-
-export let fullExample = () => {
-  let open = observable.box(true)
-  return (
-    <div style={{ height: 240 }}>
-      <Button icon="keyboard_arrow_down" onClick={F.on(open)}>
-        dropdown
-      </Button>
-      <Dropdown open={open}>
-        <DropdownItem>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </DropdownItem>
-        <DropdownItem disabled>
-          Aliquam nec fermentum lorem, vel facilisis nulla
-        </DropdownItem>
-        <Divider margin={0.5} />
-        <DropdownItem icon="eco">Etiam vitae mi elit</DropdownItem>
-        <DropdownItem icon="face">In eu maximus felis</DropdownItem>
-        <DropdownItem truncate icon="favorite">
-          Pellentesque a elit sodales, gravida magna sit amet, lacinia odio
-        </DropdownItem>
-      </Dropdown>
-    </div>
-  )
-}
+export let fullExample = () => (
+  <div style={{ height: 240 }}>
+    <Dropdown label="test" trigger="icon" triggerProps={open => ({ primary: true, icon: open ? 'face' : 'eco'})}>
+      <DropdownItem>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </DropdownItem>
+      <DropdownItem disabled>
+        Aliquam nec fermentum lorem, vel facilisis nulla
+      </DropdownItem>
+      <Divider margin={0.5} />
+      <DropdownItem icon="eco">Etiam vitae mi elit</DropdownItem>
+      <DropdownItem icon="face">In eu maximus felis</DropdownItem>
+      <DropdownItem truncate icon="favorite">
+        Pellentesque a elit sodales, gravida magna sit amet, lacinia odio
+      </DropdownItem>
+    </Dropdown>
+  </div>
+)
 
 export let minWidth = () => (
   <div style={{ height: 80 }}>
-    <ButtonDropdown isOpen={true}>
+    <Dropdown>
       <DropdownItem>A</DropdownItem>
       <DropdownItem>B</DropdownItem>
-    </ButtonDropdown>
+    </Dropdown>
   </div>
 )
 
 export let maxWidth = () => (
   <div style={{ height: 220 }}>
-    <ButtonDropdown isOpen={true}>
+    <Dropdown>
       <DropdownItem>
         Cras condimentum nulla massa, ut fermentum urna molestie eu. Phasellus
         dictum faucibus enim, eu tempus nulla efficitur nec.
@@ -62,6 +42,6 @@ export let maxWidth = () => (
         Quisque consectetur viverra dictum. In hac habitasse platea dictumst.
         Praesent purus mauris, laoreet non laoreet non, malesuada et urna.
       </DropdownItem>
-    </ButtonDropdown>
+    </Dropdown>
   </div>
 )
