42dcb59730b7ad87edad7e26ed36ef351aa22265
Michelle Saad <git@michsa.me>
2020-02-11 16:47:44 -0500

    add a couple Link stories

--- src/stories/LinkText.stories.js
+++ src/stories/LinkText.stories.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { LinkText } from '..'
+import { LinkText, Subtitle } from '..'
 
 let click = action('clicked')
 
 export default { title: 'LinkText', component: LinkText }
 
-export let story = () => <div>To delete your account, please <LinkText onClick={() => click()}>click here</LinkText>.</div>
+export let story = () => <LinkText onClick={() => click()}>Click me</LinkText>
+
+export let inline = () => (
+  <Subtitle small>
+    To delete your account, please <LinkText>click here</LinkText>.
+  </Subtitle>
+)
+
+export let inlineButton = () => (
+  <Subtitle small>
+    To delete your account, please <LinkText as="button">click here</LinkText>.
+  </Subtitle>
+)
