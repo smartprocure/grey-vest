5f233322d36f70dfbda2bc1da9a6c3503bf5db73
Michelle Saad <git@michsa.me>
2020-02-10 22:31:16 -0500

    cleanup

--- src/Popover.js
+++ src/Popover.js
@@ -1,53 +1,12 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import { useState, forwardRef, cloneElement } from 'react'
+import { forwardRef } from 'react'
 import F from 'futil'
+import _ from 'lodash/fp'
 import Box from './Box'
 import Button from './Button'
 import IconButton from './IconButton'
 import theme from './theme'
-
-// import { Manager, Reference, Popper } from 'react-popper'
-
-// export let Popover = ({
-//   label = 'dropdown',
-//   side = 'left',
-//   trigger = 'button',
-//   triggerProps,
-//   children,
-// }) => {
-//   let open = useState(false)
-//   let allTriggerProps = {
-//     onClick: F.flip(open),
-//     icon:
-//       trigger === 'button'
-//         ? `arrow_drop_${F.view(open) ? 'up' : 'down'}`
-//         : 'more_vert',
-//     ...F.callOrReturn(triggerProps, F.view(open)),
-//   }
-//   let Trigger = trigger === 'button' ? Button : IconButton
-//   return (
-//     <Manager>
-//       <Reference>
-//         {({ ref }) => (
-//           <Trigger {...{ ref, ...allTriggerProps }}>{label}</Trigger>
-//         )}
-//       </Reference>
-//       <Popper placement={`bottom-${side === 'right' ? 'end' : 'start'}`}>
-//         {({ ref, style }) =>
-//           F.view(open) && (
-//             <Box
-//               popup
-//               padding={0}
-//               css={{ marginTop: 4 }}
-//               {...{ ref, style, children }}
-//             />
-//           )
-//         }
-//       </Popper>
-//     </Manager>
-//   )
-// }
 
 import TooltipTrigger from 'react-popper-tooltip'
 
@@ -55,10 +14,8 @@ let PopupBox = forwardRef((props, ref) => (
   <Box popup paddingX={2} paddingY={1} ref={ref} {...props} />
 ))
 
-// TODO: expand the API for triggerProps and tooltipProps to include functions
+// TODO: expand the API for triggerProps and popupProps to include functions
 // of `isToggled` to props in addition to a props object
-
-// Note: we use triggerProps and tooltipProps mostly because the component
 export let Popover = ({
   Trigger = 'div',
   triggerProps = {},
@@ -99,19 +56,17 @@ export let Popover = ({
 export let Dropdown = ({
   trigger = 'button',
   label = 'dropdown',
-  icon,
-  buttonProps,
+  icon = { button: 'arrow_drop_down', icon: 'more_vert' }[trigger],
   ...props
-}) => (
-  <Popover
-    Trigger={{ button: Button, icon: IconButton }[trigger]}
-    triggerProps={{
-      children: label,
-      icon: icon || { button: 'arrow_drop_down', icon: 'more_vert' }[trigger],
-      ...buttonProps,
-    }}
-    Popup={Box}
-    popupProps={{ popup: true, padding: 0 }}
-    {...props}
-  />
-)
+}) => {
+  let parentProps = ['placement', 'side', 'children', 'isOpen', 'onClose']
+  return (
+    <Popover
+      Trigger={{ button: Button, icon: IconButton }[trigger]}
+      triggerProps={{ children: label, icon, ..._.omit(parentProps, props) }}
+      Popup={Box}
+      popupProps={{ popup: true, padding: 0 }}
+      {..._.pick(parentProps, props)}
+    />
+  )
+}
