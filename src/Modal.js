8f3de86cbae21f6e93b0605a67776fa309f20d1e
Michelle Saad <git@michsa.me>
2020-02-17 12:42:34 -0500

    convert all numeric theme size margins to aliases

--- src/Modal.js
+++ src/Modal.js
@@ -35,7 +35,7 @@ export let Modal = _.flow(
       >
         <Box
           modal
-          padding={3}
+          padding={theme.space(3)}
           onClick={e => e.stopPropagation()}
           css={{ minWidth: 400, maxWidth: 600, position: 'relative' }}
           {...props}
