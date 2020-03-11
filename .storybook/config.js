7eee1de8add68ca4b99f812f04ff2207a3c85642
Michelle Saad <git@michsa.me>
2020-03-10 20:28:19 -0400

    add conversion functions to process prop objects

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,5 @@
+import _ from 'lodash/fp'
+import F from 'futil'
 import { configure, addParameters, addDecorator } from '@storybook/react'
 import { DocsPage } from '@storybook/addon-docs/blocks'
 import Fonts from '../src/Fonts'
@@ -10,16 +12,36 @@ addDecorator(Story => (
   </>
 ))
 
+let convertType = F.unless(
+  _.overEvery(_.isObject, _.has('summary')),
+  summary => ({ summary })
+)
+
+let convertTypes = F.expandObject(
+  _.flow(
+    _.pick(['type', 'defaultValue']),
+    _.mapValues(convertType)
+  )
+)
+
+let convertProps = _.flow(
+  F.unkeyBy('name'),
+  _.map(convertTypes)
+)
+
 // DocsPage's propsSlot supplies the data for its Props component, and extractComponentDescription
 // does the same for the Description slot. This customization tells storybook to pull data from
 // `parameters.props` for props and `parameters.info` for the description, rather than from JSDoc.
 // We do it this way because Storybook's PropTypes/JSDoc parser doesn't work very well with most
-// of our components.
+// of our components (generally on account of HOC wrappers like mobx's observer).
 addParameters({
   docs: {
     page: () => (
       <DocsPage
-        propsSlot={({ parameters: { props = { rows: [] } } }) => props}
+        propsSlot={({ parameters: { props = {}, propGroups } }) => ({
+          rows: convertProps(props),
+          ...propGroups && { sections: _.mapValues(convertProps, propGroups) },
+        })}
       />
     ),
     extractComponentDescription: (target, { info }) => info,
