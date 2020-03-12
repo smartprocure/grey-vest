0f71ad0793e29b83002ba480b998dc34c875d90b
Michelle Saad <git@michsa.me>
2020-03-12 15:27:38 -0400

    add divider and type config to Controls

--- src/stories/utils.js
+++ src/stories/utils.js
@@ -1,19 +1,23 @@
 import { loremIpsum } from 'lorem-ipsum'
 import F from 'futil'
+import _ from 'lodash/fp'
 import React from 'react'
-import { Flex, FormField } from '..'
+import { Flex, FormField, Divider } from '..'
 
 export let optionsFromArray = F.mapIndexed((label, value) => ({ label, value }))
 
 export let lipsum = (count, units) => loremIpsum({ count, units })
 
-export let Controls = ({ state }) => (
-  <Flex gap="sm" justifyContent="space-evenly">
-    {F.mapIndexed(
-      (lens, key) => (
-        <FormField label={key} type="number" {...F.domLens.value(lens)} />
-      ),
-      state
-    )}
-  </Flex>
+export let Controls = ({ state, types }) => (
+  <>
+    <Flex gap="sm" justifyContent="space-evenly">
+      {F.mapIndexed(
+        (lens, key) => (
+          <FormField label={key} type={_.getOr('number', key, types)} {...F.domLens.value(lens)} />
+        ),
+        state
+      )}
+    </Flex>
+    <Divider margin="md" />
+  </>
 )
