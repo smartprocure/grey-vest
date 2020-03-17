a72fecdeb61c2ed85f6eb8d94562e21d0703aef1
Michelle Saad <git@michsa.me>
2020-03-17 16:02:28 -0400

    update/expand checkbox stories with labels & props

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -1,15 +1,57 @@
 import React from 'react'
 import { Checkbox } from '..'
+import { setDescription, setName } from './utils'
 import F from 'futil'
 
-export default { title: 'Checkbox', component: Checkbox }
-
-export let interactible = () => {
-  let checked = React.useState(false)
-  return <Checkbox {...F.domLens.targetBinding('checked')(checked)} />
+let props = {
+  checked: { type: 'boolean' },
+  onChange: {
+    type: {
+      summary: 'function',
+      detail: '(checked: boolean) => void',
+    },
+  },
+  label: {
+    type: 'string',
+    description:
+      'Optional; if this is omitted, only the checkbox is rendered. Clicking the label toggles the checkbox state.',
+  },
+  disabled: { type: 'boolean', defaultValue: 'false' },
 }
+
+export default { title: 'Checkbox', component: Checkbox, parameters: { props } }
+
+export let baseUsage = () => {
+  let [checked, setChecked] = React.useState(true)
+  return <Checkbox checked={checked} onChange={setChecked} label="Check me!" />
+}
+
+export let withDomLens = () => {
+  let checkedLens = React.useState(false)
+  return (
+    <Checkbox
+      {...F.domLens.targetBinding('checked')(checkedLens)}
+      label="Check me!"
+    />
+  )
+}
+setName(withDomLens, 'With domLens')
 
 export let unchecked = () => <Checkbox />
 export let checked = () => <Checkbox checked />
-export let disabled = () => <Checkbox disabled />
-export let disabledChecked = () => <Checkbox disabled checked />
+
+export let disabled = () => {
+  let [checked, setChecked] = React.useState(true)
+  return (
+    <Checkbox
+      disabled
+      checked={checked}
+      onChange={setChecked}
+      label="Disabled label"
+    />
+  )
+}
+setDescription(disabled, `
+A disabled checkbox should not handle click events. We attach state to the
+checkbox in this story to confirm that it will *not* be updated.
+`)
