56ce5322a25147f77f70acaa29567780987f8116
Michelle Saad <git@michsa.me>
2020-03-17 15:57:00 -0400

    Checkbox: add support for labels (and update CheckboxList)

--- src/CheckboxList.js
+++ src/CheckboxList.js
@@ -5,28 +5,16 @@ import F from 'futil'
 import { observer } from 'mobx-react'
 import Checkbox from './Checkbox'
 import ColumnList from './ColumnList'
-import Flex from './Flex'
-import { Text } from './Typography'
 
 let CheckboxList = ({ options, value, onChange, ...props }) => (
   <ColumnList gap="sm" {...props}>
     {_.map(
       option => (
-        <Flex
-          as="label"
+        <Checkbox
           key={option.value}
-          css={{ cursor: option.disabled ? 'not-allowed' : 'pointer' }}
-          gap="sm"
-        >
-          <Checkbox
-            css={{ marginTop: 3 }}
-            {...F.domLens.checkboxValues(option.value, [value, onChange])}
-            {...option}
-          />
-          <Text small css={{ flex: 1, opacity: option.disabled && 0.5 }}>
-            {option.label}
-          </Text>
-        </Flex>
+          {...F.domLens.checkboxValues(option.value, [value, onChange])}
+          {...option}
+        />
       ),
       options
     )}
